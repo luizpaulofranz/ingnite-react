5b0c522d8a34ddcde61f44b83a7f5df06de12e89
luizpaulofranz <luizpaulofranz@gmail.com>
2021-03-31 20:35:24 -0300

    Adds http fetch to populate our list.

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,20 +1,28 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React.',
-    link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList() {
+
+    // hook useState, retorna dois parametros, um eh a variavel em questão o outro eh o callBack q devemos executar para alterar essa vairavel
+    // recebe como parametro o valor inicial da variavel que criamos repositories
+    // funciona como o setState do react antigo, reexecuta essa function
+    const [repositories, setRepositories] = useState([]);
+
+    // useEffect substitui os antigos lifeCycle do react, primeiro parametro a function a ser chamada, o segundo a lista de 
+    // variaveis que quando alteradas disparam o callBack, se passarmos uma lista vazia funciona como o componentDidMount, executa uma unica vez
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then( response => response.json())
+        .then( data => setRepositories(data))
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                { repositories.map(repository => <RepositoryItem key={repository.name} repository={repository} />) }
             </ul>
         </section>
     );
